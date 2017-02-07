bc91fd5402533bab340c99e4c1679caef097a2cb
Amr Adel <amradelkhalil@gmail.com>
2017-02-07 14:23:26 +0200

    updating index designs

---
+++ app/assets/javascripts/actors/users.coffee.js
@@ -0,0 +1,27 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+// i convert it to file.coffee.js because of this (http://guides.rubyonrails.org/asset_pipeline.html#preprocessing)
+// summary: file.coffee.js compiles the file to js then to coffee and send it to browser as js
+// if you swap them you will get into problems see this too (http://stackoverflow.com/questions/38051297/rails-4-require-tree-in-application-js-doesnt-include-my-js-file-in-a)
+function setVisibleHide(dayName, button){
+
+    if(button.id == "arrow_drop_slots_right"){
+        var arr = document.getElementsByName(dayName.id);
+        var sz = document.getElementsByName(dayName.id).length;
+
+        for(var i = 0 ; i < sz ; i++){
+            arr[i].classList.remove("hiddenDay");
+        }
+
+        button.id = "arrow_drop_slots_down";
+    }else{
+        var sz = document.getElementsByName(dayName.id).length;
+        var arr = document.getElementsByName(dayName.id);
+
+        for(var i = 0 ; i < sz ; i++)
+            arr[i].className += " hiddenDay";
+
+        button.id = "arrow_drop_slots_right";
+    }
+}
