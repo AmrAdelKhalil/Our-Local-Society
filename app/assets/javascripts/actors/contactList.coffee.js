39dd363953a9da5ce14f4e8016274cd1c58e22bc
Amr Adel <amradelkhalil@gmail.com>
2017-02-21 20:30:09 +0200

    finishing contact page

--- app/assets/javascripts/actors/contactList.coffee.js
+++ app/assets/javascripts/actors/contactList.coffee.js
@@ -1,21 +1,22 @@
+function searchForElement(childs, childrensSZ, str){
+  var element;
+  for(var i = 0 ; i < childrensSZ; i++){
+    if(childs[i].tagName == str){
+      element = childs[i];
+    }
+  }
+  return element;
+}
 function addNewFriend(btn){
   var childs = btn.parentNode.children;
   var childrensSZ = childs.length;
-  var inputChild;
-  for(var i = 0 ; i < childrensSZ; i++){
-    if(childs[i].tagName == 'INPUT'){
-      inputChild = childs[i];
-    }
-  }
+  var inputChild = searchForElement(childs, childrensSZ, 'INPUT');
   callServiceAddFriend(inputChild,"http://localhost:3000/actors/service/addFriend/");//23mel api lel addFriend in rails
 }
 
 function callServiceAddFriend(inputChild, url){
       var request= new XMLHttpRequest();
-
       url  = url + inputChild.value;
-      alert(url);
-
       request.open("GET",url,true);
       //next line for post requests
       //request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
@@ -29,28 +30,35 @@ function callServiceAddFriend(inputChild, url){
 }
 
 function moveNonFriendToFriend(inputChild) {
-  alert("hello");
+  friendBoolDiv = document.getElementById("friendBool");
+  var div = document.createElement('div');
+  div.className = "col-lg-3";
+  var btn;
+
+  parent = inputChild.parentNode;
+  grandBa = parent.parentNode;
+  var childs = parent.children;
+  var childrensSZ = childs.length;
+
+  btn = searchForElement(childs, childrensSZ, 'BUTTON');
+  btn.innerHTML = 'remove'; btn.classList.remove("btn-success"); btn.className += " btn-danger"; btn.onclick = function(){removeFriend(this)};
+  div.appendChild(parent);
+
+  friendBoolDiv.appendChild(div);
+  grandBa.parentNode.removeChild(grandBa);
 }
 
 
 function removeFriend(btn){
   var childs = btn.parentNode.children;
   var childrensSZ = childs.length;
-  var inputChild;
-  for(var i = 0 ; i < childrensSZ; i++){
-    if(childs[i].tagName == 'INPUT'){
-      inputChild = childs[i];
-    }
-  }
+  var inputChild = searchForElement(childs, childrensSZ, 'INPUT');
   callServiceremoveFriend(inputChild,"http://localhost:3000/actors/service/removeFriend/");//23mel api lel addFriend in rails
 }
 
 function callServiceremoveFriend(inputChild, url){
       var request= new XMLHttpRequest();
-
       url  = url + inputChild.value;
-      alert(url);
-
       request.open("GET",url,true);
       //next line for post requests
       //request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
@@ -64,5 +72,20 @@ function callServiceremoveFriend(inputChild, url){
 }
 
 function moveFriendToNonFriend(inputChild) {
-  alert("hello");
+  nonFriendBoolDiv = document.getElementById("nonFriendBool");
+  var div = document.createElement('div');
+  div.className = "col-lg-3";
+  var btn;
+  
+  parent = inputChild.parentNode;
+  var childs = parent.children;
+  var childrensSZ = childs.length;
+
+  btn = searchForElement(childs, childrensSZ, 'BUTTON');
+  btn.innerHTML="add"; btn.classList.remove("btn-danger"); btn.className += " btn-success"; btn.onclick = function(){addNewFriend(this)};
+  grandBa = parent.parentNode;
+  div.appendChild(parent);
+
+  nonFriendBoolDiv.appendChild(div);
+  grandBa.parentNode.removeChild(grandBa);
 }
